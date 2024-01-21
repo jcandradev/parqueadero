15a1007e0a174968f9cae7a370e126f772b06aa8
Jan Carlos Andrade <jcandrade679@soy.sena.edu.co>
2024-01-20 22:31:05 -0500

    feat: :sparkles: Agregamos enpoints para listar, eliminar y actualizar parques

--- routes/parque.route.js
+++ routes/parque.route.js
@@ -1,8 +1,11 @@
 import { Router } from "express";
-import { registroParque } from "../controllers/parque.controller.js"
+import { registroParque, listarParques, eliminarParque, actualizarParque } from "../controllers/parque.controller.js"
 
 const router = Router();
 
-router.post("/registro", registroParque);
+router.post("/registro", registroParque)
+router.get("/listar", listarParques)
+router.delete("/eliminar", eliminarParque)
+router.put("/actualizar", actualizarParque)
 
 export default router;
