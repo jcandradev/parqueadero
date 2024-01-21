99cc5225b6796181b04e0f20e9d8bd6ccf2f1e85
Jan Carlos Andrade <jcandrade679@soy.sena.edu.co>
2024-01-20 22:30:21 -0500

    refactor: :recycle: Se elimina :

--- database/connectdb.js
+++ database/connectdb.js
@@ -6,7 +6,7 @@ dotenv.config();
 try {
   mongoose.set("strictQuery", false)
   mongoose.connect(process.env.MONGO_URI)
-  console.log(">>> Base de Datos Conectada");
+  console.log(">>> Base de Datos Conectada")
 } catch (error) {
   console.log("Error al conectar la base de datos", error);
   process.exit(1);
